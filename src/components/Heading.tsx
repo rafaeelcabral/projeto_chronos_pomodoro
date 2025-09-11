c0c4f68c5f17847509141f108e1de509783be606
Rafael Cabral <melloraf.cabral@gmail.com>
2025-09-11 10:11:54 -0300

    Definindo variáveis de cor para tema escuro e instalando a biblioteca de ícones lucide

---
+++ src/components/Heading.tsx
@@ -0,0 +1,19 @@
+import styles from './Heading.module.css';
+
+//Tipando a Propriedade do Componente 'props' por causa do Ts
+//Tudo q o React aceita como Children => React.ReactNode
+type HeadingProps= {
+    children: React.ReactNode; 
+}
+
+function Heading(props: HeadingProps) {
+
+    return ( 
+
+        <h1 className={styles.heading}> {props.children} </h1> 
+        
+    );
+
+}
+
+export { Heading };
